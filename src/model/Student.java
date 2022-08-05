edc0c8a0ec2d29159809aedd8699fffea2f5c1d5
Gunsmoke017 <Percivalrp106@gmail.com>
2022-08-05 11:56:49 +0100

    Model student created

---
+++ src/model/Student.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Student {
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String dob;
+    private long phoneNumber;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
